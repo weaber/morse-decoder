ebef2ad419aa7db05e702ff1b30e09d3c6ac3b69
weaber <shirchenko_d@mail.ru>
2023-01-29 12:23:53 +0300

    init: task solved

--- src/index.js
+++ src/index.js
@@ -37,8 +37,51 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const codesKeys = Object.keys(MORSE_TABLE);
+
+const encodedKeys = codesKeys.map((item) => {
+    let code ='';
+    let zeros = '';
+
+    for (let i = 0; i < item.length; i++) {
+        if (item[i] === '-') {
+        code = code.concat('11');
+        } else if (item[i] === '.') {
+        code = code.concat('10');}    
+        
+    }
+    if (code.length < 10) {
+        let lettersLeft = 10 - code.length;    
+        for (let i = 0; i < lettersLeft; i++) {
+        zeros = zeros.concat('0');
+        }
+    }
+    return zeros.concat(code);
+});
+
+const getLetters = (string) => {
+    const LETTER_LENGTH = 10;
+    const lettersArr = [];
+    
+    for (let i = 0; i < string.length; i += LETTER_LENGTH) {        
+      lettersArr.push(string.substr(i, LETTER_LENGTH));
+    }
+    return lettersArr;
+  };
+
+
+
 function decode(expr) {
-    // write your solution here
+    const codedMessage = getLetters(expr);
+    const lettersNumbers =[];
+    codedMessage.forEach((item) => {
+        lettersNumbers.push(encodedKeys.indexOf(item));
+    })
+    const decodedMessage = lettersNumbers.map((item) => {
+        return item === -1 ? " " : MORSE_TABLE[codesKeys[item]];  
+      });
+    const answer = decodedMessage.join('');
+    return answer;
 }
 
 module.exports = {
